e79f4962ebe94c1bdfc2f835991be067f37fd84b
imjae <code@kakao.com>
2021-05-06 10:51:02 +0900

    first spring gradle enviroment setting

---
+++ src/main/java/hello/hello_sping/HelloSpingApplication.java
@@ -0,0 +1,13 @@
+package hello.hello_sping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpingApplication.class, args);
+	}
+
+}
