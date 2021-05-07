af5324f2cfe3a538d070e23a9961de46767b574d
imjae <code@kakao.com>
2021-05-07 09:21:12 +0900

    회원 도메인과 리포지토리 만들기

---
+++ src/main/java/hello/hello_sping/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package hello.hello_sping.repository;
+
+import hello.hello_sping.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
+    
+}
